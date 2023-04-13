a2297001cc03bba7887c929d3057b06f9674fb3a
MohammadAlimul <mohammad4limul@gmail.com>
2023-04-13 21:32:22 +0700

    Vector2f multiplication

---
+++ include/util/operator_overload.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "SFML/System/Vector2.hpp"
+
+namespace sf 
+{
+    Vector2f operator*(const Vector2f& left, const Vector2f& right);
+}
---
+++ src/util/operator_overload.cpp
@@ -0,0 +1,9 @@
+#include "SFML/System/Vector2.hpp"
+
+namespace sf
+{
+    Vector2f operator*(const Vector2f& left, const Vector2f& right)
+    {
+        return Vector2f(left.x * right.x, left.y * right.y);
+    }
+}

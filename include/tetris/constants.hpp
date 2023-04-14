8a12f0d6d558f05f0def56214ef0692cebcc8947
MohammadAlimul <mohammad4limul@gmail.com>
2023-04-14 09:03:07 +0700

    use constats for tile vertices

--- include/tetris/constants.hpp
+++ include/tetris/constants.hpp
@@ -3,9 +3,18 @@
 
 #include "SFML/System/Vector2.hpp"
 
-const unsigned int WINDOW_WIDTH = 480;
-const unsigned int WINDOW_HEIGHT = 640;
+static inline constexpr unsigned int WINDOW_WIDTH = 480;
+static inline constexpr unsigned int WINDOW_HEIGHT = 640;
 
-inline const sf::Vector2f TILE_SIZE = sf::Vector2f(20., 20.);
+static inline const sf::Vector2f TILE_SIZE = sf::Vector2f(20., 20.);
+
+static inline const sf::Vector2f P1 = sf::Vector2f(1.0f, 1.0f);
+static inline const sf::Vector2f P2 = sf::Vector2f(2.0f, 1.0f);
+static inline const sf::Vector2f P3 = sf::Vector2f(2.0f, 2.0f);
+static inline const sf::Vector2f P4 = sf::Vector2f(1.0f, 2.0f);
+static inline const sf::Vector2f P5 = sf::Vector2f(1.2f, 1.2f);
+static inline const sf::Vector2f P6 = sf::Vector2f(1.8f, 1.2f);
+static inline const sf::Vector2f P7 = sf::Vector2f(1.8f, 1.8f);
+static inline const sf::Vector2f P8 = sf::Vector2f(1.2f, 1.8f);
 
 #endif
--- src/tetris/game/entity/tile.cpp
+++ src/tetris/game/entity/tile.cpp
@@ -1,60 +1,61 @@
 #include "tetris/entity/tile.hpp"
 #include "util/operator_overload.hpp"
+#include "tetris/constants.hpp"
 
 void Tile::initVertices()
 {
-    this->vertecies[0]  = sf::Vertex(this->position + this->size * sf::Vector2f(1.2f, 1.2f), adjustColor(this->color, -20, 1));
-    this->vertecies[1]  = sf::Vertex(this->position + this->size * sf::Vector2f(1.8f, 1.2f), adjustColor(this->color, -20, 1));
-    this->vertecies[2]  = sf::Vertex(this->position + this->size * sf::Vector2f(1.8f, 1.8f), adjustColor(this->color, -20, 1));
-    this->vertecies[3]  = sf::Vertex(this->position + this->size * sf::Vector2f(1.2f, 1.8f), adjustColor(this->color, -20, 1));
+    this->vertecies[0]  = sf::Vertex(this->position + this->size * P5, adjustColor(this->color, -20, 1));
+    this->vertecies[1]  = sf::Vertex(this->position + this->size * P6, adjustColor(this->color, -20, 1));
+    this->vertecies[2]  = sf::Vertex(this->position + this->size * P7, adjustColor(this->color, -20, 1));
+    this->vertecies[3]  = sf::Vertex(this->position + this->size * P8, adjustColor(this->color, -20, 1));
 
-    this->vertecies[4]  = sf::Vertex(this->position + this->size * sf::Vector2f(1.0f, 1.0f), adjustColor(this->color, -20, 0));
-    this->vertecies[5]  = sf::Vertex(this->position + this->size * sf::Vector2f(2.0f, 1.0f), adjustColor(this->color, -20, 0));
-    this->vertecies[6]  = sf::Vertex(this->position + this->size * sf::Vector2f(1.8f, 1.2f), adjustColor(this->color, -20, 0));
-    this->vertecies[7]  = sf::Vertex(this->position + this->size * sf::Vector2f(1.2f, 1.2f), adjustColor(this->color, -20, 0));
+    this->vertecies[4]  = sf::Vertex(this->position + this->size * P1, adjustColor(this->color, -20, 0));
+    this->vertecies[5]  = sf::Vertex(this->position + this->size * P2, adjustColor(this->color, -20, 0));
+    this->vertecies[6]  = sf::Vertex(this->position + this->size * P6, adjustColor(this->color, -20, 0));
+    this->vertecies[7]  = sf::Vertex(this->position + this->size * P5, adjustColor(this->color, -20, 0));
 
-    this->vertecies[8]  = sf::Vertex(this->position + this->size * sf::Vector2f(1.0f, 1.0f), adjustColor(this->color, -20, 2));
-    this->vertecies[9]  = sf::Vertex(this->position + this->size * sf::Vector2f(1.0f, 2.0f), adjustColor(this->color, -20, 2));
-    this->vertecies[10] = sf::Vertex(this->position + this->size * sf::Vector2f(1.2f, 1.8f), adjustColor(this->color, -20, 2));
-    this->vertecies[11] = sf::Vertex(this->position + this->size * sf::Vector2f(1.2f, 1.2f), adjustColor(this->color, -20, 2));
+    this->vertecies[8]  = sf::Vertex(this->position + this->size * P1, adjustColor(this->color, -20, 2));
+    this->vertecies[9]  = sf::Vertex(this->position + this->size * P4, adjustColor(this->color, -20, 2));
+    this->vertecies[10] = sf::Vertex(this->position + this->size * P8, adjustColor(this->color, -20, 2));
+    this->vertecies[11] = sf::Vertex(this->position + this->size * P5, adjustColor(this->color, -20, 2));
     
-    this->vertecies[12] = sf::Vertex(this->position + this->size * sf::Vector2f(2.0f, 1.0f), adjustColor(this->color, -20, 3));
-    this->vertecies[13] = sf::Vertex(this->position + this->size * sf::Vector2f(2.0f, 2.0f), adjustColor(this->color, -20, 3));
-    this->vertecies[14] = sf::Vertex(this->position + this->size * sf::Vector2f(1.8f, 1.8f), adjustColor(this->color, -20, 3));
-    this->vertecies[15] = sf::Vertex(this->position + this->size * sf::Vector2f(1.8f, 1.2f), adjustColor(this->color, -20, 3));
+    this->vertecies[12] = sf::Vertex(this->position + this->size * P2, adjustColor(this->color, -20, 3));
+    this->vertecies[13] = sf::Vertex(this->position + this->size * P3, adjustColor(this->color, -20, 3));
+    this->vertecies[14] = sf::Vertex(this->position + this->size * P7, adjustColor(this->color, -20, 3));
+    this->vertecies[15] = sf::Vertex(this->position + this->size * P6, adjustColor(this->color, -20, 3));
 
-    this->vertecies[16] = sf::Vertex(this->position + this->size * sf::Vector2f(1.0f, 2.0f), adjustColor(this->color, -20, 4));
-    this->vertecies[17] = sf::Vertex(this->position + this->size * sf::Vector2f(2.0f, 2.0f), adjustColor(this->color, -20, 4));
-    this->vertecies[18] = sf::Vertex(this->position + this->size * sf::Vector2f(1.8f, 1.8f), adjustColor(this->color, -20, 4));
-    this->vertecies[19] = sf::Vertex(this->position + this->size * sf::Vector2f(1.2f, 1.8f), adjustColor(this->color, -20, 4));
+    this->vertecies[16] = sf::Vertex(this->position + this->size * P4, adjustColor(this->color, -20, 4));
+    this->vertecies[17] = sf::Vertex(this->position + this->size * P3, adjustColor(this->color, -20, 4));
+    this->vertecies[18] = sf::Vertex(this->position + this->size * P7, adjustColor(this->color, -20, 4));
+    this->vertecies[19] = sf::Vertex(this->position + this->size * P8, adjustColor(this->color, -20, 4));
 }
 
 void Tile::adjustVerticesPos()
 {
-    this->vertecies[0].position  = this->position + this->size * sf::Vector2f(1.2f, 1.2f);
-    this->vertecies[1].position  = this->position + this->size * sf::Vector2f(1.8f, 1.2f);
-    this->vertecies[2].position  = this->position + this->size * sf::Vector2f(1.8f, 1.8f);
-    this->vertecies[3].position  = this->position + this->size * sf::Vector2f(1.2f, 1.8f);
-                                 
-    this->vertecies[4].position  = this->position + this->size * sf::Vector2f(1.0f, 1.0f);
-    this->vertecies[5].position  = this->position + this->size * sf::Vector2f(2.0f, 1.0f);
-    this->vertecies[6].position  = this->position + this->size * sf::Vector2f(1.8f, 1.2f);
-    this->vertecies[7].position  = this->position + this->size * sf::Vector2f(1.2f, 1.2f);
-                                 
-    this->vertecies[8].position  = this->position + this->size * sf::Vector2f(1.0f, 1.0f);
-    this->vertecies[9].position  = this->position + this->size * sf::Vector2f(1.0f, 2.0f);
-    this->vertecies[10].position = this->position + this->size * sf::Vector2f(1.2f, 1.8f);
-    this->vertecies[11].position = this->position + this->size * sf::Vector2f(1.2f, 1.2f);
-    
-    this->vertecies[12].position = this->position + this->size * sf::Vector2f(2.0f, 1.0f);
-    this->vertecies[13].position = this->position + this->size * sf::Vector2f(2.0f, 2.0f);
-    this->vertecies[14].position = this->position + this->size * sf::Vector2f(1.8f, 1.8f);
-    this->vertecies[15].position = this->position + this->size * sf::Vector2f(1.8f, 1.2f);
-
-    this->vertecies[16].position = this->position + this->size * sf::Vector2f(1.0f, 2.0f);
-    this->vertecies[17].position = this->position + this->size * sf::Vector2f(2.0f, 2.0f);
-    this->vertecies[18].position = this->position + this->size * sf::Vector2f(1.8f, 1.8f);
-    this->vertecies[19].position = this->position + this->size * sf::Vector2f(1.2f, 1.8f);
+    this->vertecies[0].position  = this->position + this->size * P5;
+    this->vertecies[1].position  = this->position + this->size * P6;
+    this->vertecies[2].position  = this->position + this->size * P7;
+    this->vertecies[3].position  = this->position + this->size * P8;
+                                                                   
+    this->vertecies[4].position  = this->position + this->size * P1;
+    this->vertecies[5].position  = this->position + this->size * P2;
+    this->vertecies[6].position  = this->position + this->size * P6;
+    this->vertecies[7].position  = this->position + this->size * P5;
+                                                                   
+    this->vertecies[8].position  = this->position + this->size * P1;
+    this->vertecies[9].position  = this->position + this->size * P4;
+    this->vertecies[10].position = this->position + this->size * P8;
+    this->vertecies[11].position = this->position + this->size * P5;
+                                                                   
+    this->vertecies[12].position = this->position + this->size * P2;
+    this->vertecies[13].position = this->position + this->size * P3;
+    this->vertecies[14].position = this->position + this->size * P7;
+    this->vertecies[15].position = this->position + this->size * P6;
+                                                                   
+    this->vertecies[16].position = this->position + this->size * P4;
+    this->vertecies[17].position = this->position + this->size * P3;
+    this->vertecies[18].position = this->position + this->size * P7;
+    this->vertecies[19].position = this->position + this->size * P8;
 }
 
 Tile::Tile(sf::Vector2f initPos, sf::Vector2f size, sf::Color color, sf::Vector2f origin) 
